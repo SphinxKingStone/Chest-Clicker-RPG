bb8721f645b8aa1dea528069100e56c8212dd136
SphinxKingStone <Sphinx_King_Stone@mail.ru>
2021-08-02 06:26:45 +0300

    added 5$ asset pack

--- assets/data/items.gd
+++ assets/data/items.gd
@@ -1,14 +1,14 @@
 var weapons = {
 	simple_axe = {
 		code = "simple axe",
-		image = load("res://assets/images/items/simple axe.png"),
+		image = load("res://assets/images/axes/axe_7.png"),
 		cost = 5,
 		luck = 0.3
 	},
 	
-	simple_dagger = {
-		code = "simple dagger",
-		image = load("res://assets/images/items/simple dagger.png"),
+	wooden_sword = {
+		code = "wooden sword",
+		image = load("res://assets/images/swords/sword_6.png"),
 		cost = 3,
 		luck = 0.2
 	}
@@ -17,14 +17,14 @@ var weapons = {
 var armour = {
 	leather_chestplate = {
 		code = "leather chestplate",
-		image = load("res://assets/images/items/leather chestplate.png"),
+		image = load("res://assets/images/armors/armor_17.png"),
 		cost = 10,
 		luck = 0.7
 	},
 	
 	leather_hood = {
 		code = "leather hood",
-		image = load("res://assets/images/items/leather hood.png"),
+		image = load("res://assets/images/helmets/helmet_83.png"),
 		cost = 8,
 		luck = 0.6
 	},
