49fb0f0a6ebbec4bd998f1cbc210c20f8680b8c6
SphinxKingStone <Sphinx_King_Stone@mail.ru>
2022-05-30 05:59:49 +0300

    removing duplicate files

--- data/Colors.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-const button_background_normal = Color("0eab00")
-const button_background_pressed = Color("075400")
-const bonus_text_green = Color("0fff00")
-const bonus_text_red = Color("ff0000")
-
-func _ready():
-	pass
