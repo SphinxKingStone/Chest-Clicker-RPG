54b7805033e002431b8bc22b5e04e6a602bb647b
SphinxKingStone <Sphinx_king_stone@mail.ru>
2023-11-11 22:12:04 +0300

    a little settings change, added my audio library temporary

--- gui/UI/Scripts/SettingsScene.gd
+++ gui/UI/Scripts/SettingsScene.gd
@@ -8,10 +8,10 @@ func _ready():
 	$BackButton.connect("pressed", self, "exit_pressed")
 
 func slider_change(value_changed, slider, type):
-	var volume = (slider.value - 100) / 4
+	var volume = slider.value - 20
 	
-	# if volume slider set to minimum then mute
-	if volume <= -25:
+	# if volume slider set to minimum then mute aka make it -100
+	if volume <= -20:
 		volume = -100
 	match type:
 		"music":
@@ -21,6 +21,7 @@ func slider_change(value_changed, slider, type):
 			Settings.set_sounds_volume(volume)
 		"creak":
 			Settings.set_creak_volume(volume)
+	Audio.play_sound(preload("res://assets/Sounds/Sounds/UI/Slider_v3_wav.wav"), 0, -5)
 
 func exit_pressed():
 	hide()
